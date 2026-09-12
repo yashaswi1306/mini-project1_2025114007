f59f8f723590e2d7fe8bec5530b1d90a5eef7001
yashaswi1306 <yashaswi130607@gmail.com>
2026-09-12 14:32:55 +0530

    execute edited final ver

--- c-shell/include/execute.h
+++ c-shell/include/execute.h
@@ -3,8 +3,11 @@
 
 #include "lexer.h"
 
-void execute_cmd(const token_list_t *list);
+int execute_cmd(const token_list_t *list);
+int execute_pipeline(const token_list_t *list);
+int execute_pipeline_fg(const token_list_t *list, const char *full_cmd_str);
+int execute_pipeline_bg(const token_list_t *list, int job_id, const char *full_cmd_str);
+int execute_is_executable(const char *filepath);
 
-void execute_pipeline(const token_list_t *list);
 
 #endif
