a33a833552c4aa076485c74fbe99520dd3c2c9d4
yashaswi1306 <yashaswi130607@gmail.com>
2026-09-13 16:09:04 +0530

    schedtest update

--- xv6/user/schedtest.c
+++ xv6/user/schedtest.c
@@ -1,57 +1,58 @@
-#include "kernel/types.h"
-#include "kernel/stat.h"
-#include "user/user.h"
+#include "kernel/types.h" 
+#include "kernel/stat.h" 
+#include "user/user.h" 
 
-#define NPROCS 4
+#define NPROCS 4 // num of procs to run
 
-void cpu_bound_work(void) {
-  for (volatile int i=0; i<200000000; i++) {
-    // Heavy CPU computation
-  }
+void cpu_bound_work(void) { // func for heavy cpu work
+  for (volatile int i=0; i<200000000; i++) { //volatile so it dsnt optimize process
+    // Heavy CPU computation to check how os handles scheduler refusing to share cpu time
+  } 
 }
 
-void io_bound_work(void) {
-  for (int i=0; i<20; i++) {
-    pause(1); // Voluntary yield / sleep
-  }
-}
+void io_bound_work(void) { //fake io wait
+  for (int i=0; i<20; i++) { // loop a bit
+    pause(1); // Voluntary yield / sleep, i.e. giv up cpu to sleep
+  } 
+} 
 
-int main(int argc, char *argv[]) {
-  printf("Scheduler Benchmark Test (NPROCS=%d)\n", NPROCS);
+int main(int argc, char *argv[]) { 
+  printf("Scheduler Benchmark Test (NPROCS=%d)\n", NPROCS); // print start msg
 
-  for (int i=0; i<NPROCS; i++) {
-    int pid = fork();
-    if (pid==0) {
-      if (i%2==0) {
-        cpu_bound_work();
-      } else {
-        io_bound_work();
-      }
-      exit(0);
+  for (int i=0; i<NPROCS; i++) { 
+    int pid = fork(); // make a child proc
+    if (pid==0) { // if its the kid
+      if (i%2==0) { 
+        cpu_bound_work();  //even children do heavy comp
+      } else { 
+        io_bound_work(); // dodd children do io nad sleepoing
+      } 
+      exit(0); 
     }
-  }
+  } 
 
-  int total_rtime=0;
-  int total_wtime=0;
-  int total_ttime=0;
+  int total_rtime=0; // track total run time
+  int total_wtime=0; // track total wait time
+  int total_ttime=0; // track total turnaroud
 
-  for (int i=0; i<NPROCS; i++) {
-    int r, w;
-    int pid = waitx(0, &r, &w);
-    int turnaround = r+w;
+  for (int i=0; i<NPROCS; i++) 
+  { // loop to wait for child proc
+    int r, w; // run and wait
+    int pid = waitx(0, &r, &w); // wait for a child to fin and get stats
+    int turnaround = r+w; // calc total time from start to fin
 
-    total_rtime += r;
-    total_wtime += w;
-    total_ttime += turnaround;
+    total_rtime += r; // add run to totl
+    total_wtime += w; // add wait to totl
+    total_ttime += turnaround; // add turn to totl
 
-    printf("Process PID %d: RunTime = %d ticks, WaitTime = %d ticks, TurnaroundTime = %d ticks\n",
-           pid, r, w, turnaround);
+    printf("Process PID %d: RunTime = %d ticks, WaitTime = %d ticks, TurnaroundTime = %d ticks\n", // prnt stats for this child
+           pid, r, w, turnaround); 
   }
 
   printf("\nSummary Statistics\n");
-  printf("Average Response / Running Time: %d ticks\n", total_rtime / NPROCS);
-  printf("Average Waiting Time: %d ticks\n", total_wtime / NPROCS);
-  printf("Average Turnaround Time: %d ticks\n", total_ttime / NPROCS);
+  printf("Average Response / Running Time: %d ticks\n", total_rtime / NPROCS); // avg run time
+  printf("Average Waiting Time: %d ticks\n", total_wtime / NPROCS); // avg wait time
+  printf("Average Turnaround Time: %d ticks\n", total_ttime / NPROCS); // avg turnaroud
 
-  exit(0);
-}
+  exit(0); 
+} 
